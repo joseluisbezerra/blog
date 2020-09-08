f485b43ff64e459ff9d39561d12f3525123b72cc
Luis Bezerra <52874192+joseluisbezerra@users.noreply.github.com>
2020-09-08 07:26:03 -0300

    UpdateView e CreateView

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic.edit import CreateView
 
 from .models import Post
 
@@ -7,7 +8,16 @@ class BlogListView(ListView):
     model = Post
     template_name = 'blog/home.html'
 
-
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = ('autor', 'titulo', 'conteudo')
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ('titulo', 'conteudo')
